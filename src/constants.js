e31bc980d42a4c30deccaee93669cb135a0040a2
thedittya471 <dittyamaity@gmail.com>
2026-03-17 23:58:37 +0530

    add user role and task status enums to constants

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const UserRoleEnum = {
+    ADMIN: "admin",
+    PROJECT_ADMIN: "project-admin",
+    MEMBER: "member"
+}
+
+export const AvailbleUserRoles = Object.values(UserRoleEnum)
+
+export const TaskStatusEnum = {
+    TODO: "todo",
+    IN_PROGRESS: "in-progress",
+    DONE: "done"
+}
+
+export const AvailableTaskStatus = Object.values(TaskStatusEnum)
+
