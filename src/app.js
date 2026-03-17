25f073e0a1e1fc968001cbf69e366d4ee259c4b7
thedittya471 <dittyamaity@gmail.com>
2026-03-17 22:29:39 +0530

    add CORS middleware and update package dependencies

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("welcome to the server")
-})
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") ||  "http://localhost:8000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("welcome to the server");
+});
 
 export default app;
